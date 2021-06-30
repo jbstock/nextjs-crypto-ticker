9524d50630d8263251c81d847cda5eace3c643d4
John Stocklin <17053644+jbstock@users.noreply.github.com>
2021-06-30 16:57:57 -0400

    Coin and CoinList components and styles

---
+++ components/Coins/index.js
@@ -0,0 +1,46 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+import styles from './Coins.module.css';
+
+const Coins = ({
+  name,
+  id,
+  price,
+  symbol,
+  marketcap,
+  volume,
+  image,
+  priceChange,
+}) => {
+  return (
+    <div className={styles.coin_container}>
+      <div className={styles.coin_row}>
+        <div className={styles.coin}>
+          <img src={image} alt={name} className={styles.coin_img} />
+          <h1 className={styles.coin_h1}>{name}</h1>
+          <p className={styles.coin_symbol}>{symbol}</p>
+        </div>
+        <div className={styles.coin_data}>
+          <p className={styles.coin_price}>${price}</p>
+          <p className={styles.coin_volume}>
+            ${volume.toLocaleString()} &nbsp;
+          </p>
+          {priceChange < 0 ? (
+            <p className={(styles.coin_percent, styles.red)}>
+              ({priceChange.toFixed(2)}%)
+            </p>
+          ) : (
+            <p className={(styles.coin_percent, styles.green)}>
+              ({priceChange.toFixed(2)}%)
+            </p>
+          )}
+          <p className={styles.coin_marketcap}>
+            Mkt Cap: ${marketcap.toLocaleString()}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Coins;
