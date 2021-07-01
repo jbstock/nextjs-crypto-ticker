99c8534d42630bc5a2a750e062d431d9c49ee417
John Stocklin <17053644+jbstock@users.noreply.github.com>
2021-07-01 10:43:10 -0400

    final proof of concept - styling not perfect, desktop only

--- components/Coins/index.js
+++ components/Coins/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import styles from './Coins.module.css';
+import Link from 'next/link';
 
 const Coins = ({
   name,
@@ -13,33 +14,37 @@ const Coins = ({
   priceChange,
 }) => {
   return (
-    <div className={styles.coin_container}>
-      <div className={styles.coin_row}>
-        <div className={styles.coin}>
-          <img src={image} alt={name} className={styles.coin_img} />
-          <h1 className={styles.coin_h1}>{name}</h1>
-          <p className={styles.coin_symbol}>{symbol}</p>
+    <Link href='/coin/[id]' as={`/coin/${id}`} passHref>
+      <a>
+        <div className={styles.coin_container}>
+          <div className={styles.coin_row}>
+            <div className={styles.coin}>
+              <img src={image} alt={name} className={styles.coin_img} />
+              <h1 className={styles.coin_h1}>{name}</h1>
+              <p className={styles.coin_symbol}>{symbol}</p>
+            </div>
+            <div className={styles.coin_data}>
+              <p className={styles.coin_price}>${price}</p>
+              <p className={styles.coin_volume}>
+                ${volume.toLocaleString()} &nbsp;
+              </p>
+              {priceChange < 0 ? (
+                <p className={(styles.coin_percent, styles.red)}>
+                  ({priceChange.toFixed(2)}%)
+                </p>
+              ) : (
+                <p className={(styles.coin_percent, styles.green)}>
+                  ({priceChange.toFixed(2)}%)
+                </p>
+              )}
+              <p className={styles.coin_marketcap}>
+                Mkt Cap: ${marketcap.toLocaleString()}
+              </p>
+            </div>
+          </div>
         </div>
-        <div className={styles.coin_data}>
-          <p className={styles.coin_price}>${price}</p>
-          <p className={styles.coin_volume}>
-            ${volume.toLocaleString()} &nbsp;
-          </p>
-          {priceChange < 0 ? (
-            <p className={(styles.coin_percent, styles.red)}>
-              ({priceChange.toFixed(2)}%)
-            </p>
-          ) : (
-            <p className={(styles.coin_percent, styles.green)}>
-              ({priceChange.toFixed(2)}%)
-            </p>
-          )}
-          <p className={styles.coin_marketcap}>
-            Mkt Cap: ${marketcap.toLocaleString()}
-          </p>
-        </div>
-      </div>
-    </div>
+      </a>
+    </Link>
   );
 };
 
